f83b08096dc9f14da7996d2b08a0d02acc381668
Todd Simpson <toddsimpson@BCs-MacBook-Pro.local>
2020-10-13 14:04:54 -0500

    Completed

---
+++ variablesExercise.js
@@ -0,0 +1,19 @@
+let myAge = 17;
+
+myAge = 12;
+
+myAge += 6;
+
+myAge--;
+
+myAge = "None of your buisiness"
+
+console.log(myAge);
+
+let blank;
+blank = null;
+
+// Bonus
+
+let notANumber = 0 / 0;
+console.log(notANumber);
